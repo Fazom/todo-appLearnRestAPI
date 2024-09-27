8ba78dc850803f0cde1e13db7afbc0247c10ea64
Fazom <kbelyavskiy2015@mail.ru>
2024-09-27 19:33:08 +0300

    Добавил все хэндлеры и зависимости

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,28 @@ import (
 	"log"
 
 	"github.com/Fazom/todo-appLearnRestAPI.git"
+	"github.com/Fazom/todo-appLearnRestAPI.git/package/handler"
+	"github.com/Fazom/todo-appLearnRestAPI.git/package/repository"
+	"github.com/Fazom/todo-appLearnRestAPI.git/package/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Ошибка инициализации конфигурации: %v", err)
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- server.go
+++ server.go
@@ -10,9 +10,10 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error { //Запуск сервера на указанном порте
+func (s *Server) Run(port string, handler http.Handler) error { //Запуск сервера на указанном порте
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
+		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
