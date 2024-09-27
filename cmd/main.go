312c6f95f821fd193e432773ffc81112cd1a5346
Fazom <kbelyavskiy2015@mail.ru>
2024-09-27 16:41:32 +0300

    Создал запуск сервера и основные структуры

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/Fazom/todo-appLearnRestAPI.git"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatal(err)
+	}
+}
