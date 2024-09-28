3fb89e9190a6f5bb774faffebfc291b7d45fb0f6
Fazom <kbelyavskiy2015@mail.ru>
2024-09-28 20:59:09 +0300

    добавил авторизацию и регистрацию

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Fazom/todo-appLearnRestAPI.git"
 	"github.com/Fazom/todo-appLearnRestAPI.git/package/handler"
 	"github.com/Fazom/todo-appLearnRestAPI.git/package/repository"
 	"github.com/Fazom/todo-appLearnRestAPI.git/package/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 	if err := initConfig(); err != nil {
-		log.Fatalf("Ошибка инициализации конфигурации: %v", err)
+		logrus.Fatalf("Ошибка инициализации конфигурации: %v", err)
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatal("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
 
